c28a301bea5e0f6bc77c415910fb25e58ca61472
HectorArteagaZambrano <50024910+HectorArteagaZambrano@users.noreply.github.com>
2025-06-24 13:47:08 -0500

    [fix] - solventando error de ubicación de assets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     baseUrl: 'https://randomuser.me/api',
-    baseIcon: 'http://localhost:4200',
+    baseIcon: 'https://demo-user-management-app.netlify.app',
     secretKey: 'U53R-M4N4G3M3NT-4PP'
 };
